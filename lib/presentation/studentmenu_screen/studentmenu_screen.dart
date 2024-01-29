ccfb11643ec46c0c6e216910e5bfe1291a1fcadb
Moritz Wieser <eduwiemor002@fh-kaernten.at>
2024-01-29 21:47:17 +0100

    updated studentmenu with popup, tried on teacherlogin

--- lib/presentation/studentmenu_screen/studentmenu_screen.dart
+++ lib/presentation/studentmenu_screen/studentmenu_screen.dart
@@ -80,89 +80,135 @@ class StudentmenuScreen extends StatelessWidget {
   /// Section Widget
   Widget _buildStudentMenu(BuildContext context) {
     return Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 10.h, vertical: 25.v),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadiusStyle.customBorderTL25,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.2),
-                  spreadRadius: 2,
-                  blurRadius: 5,
-                  offset: Offset(0, 3),
-                ),
-              ],
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10.h, vertical: 25.v),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadiusStyle.customBorderTL25,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(0, 3),
             ),
-            child: Column(mainAxisSize: MainAxisSize.min, children: [
-              Text("Schüler", style: CustomTextStyles.displaySmallBold),
-              SizedBox(height: 12.v),
-              Padding(
-                padding: EdgeInsets.only(left: 8.h),
-                child: GestureDetector(
-                  onTap: () {
-                    FocusScope.of(context).requestFocus(roomNumberFocus);
-                  },
-                  child: Center(
-                    // Zentriere den Text im Container
-                    child: CustomTextFormField(
-                      controller: roomNumberController,
-                      hintText: "Raumnummer",
-                      focusNode: roomNumberFocus,
-                      autofocus: false, // Setze autofocus auf false
-                    ),
-                  ),
+          ],
+        ),
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Text("Schüler", style: CustomTextStyles.displaySmallBold),
+          SizedBox(height: 12.v),
+          Padding(
+            padding: EdgeInsets.only(left: 8.h),
+            child: GestureDetector(
+              onTap: () {
+                FocusScope.of(context).requestFocus(roomNumberFocus);
+              },
+              child: Center(
+                child: CustomTextFormField(
+                  controller: roomNumberController,
+                  hintText: "Raumnummer",
+                  focusNode: roomNumberFocus,
+                  autofocus: false,
                 ),
               ),
-              SizedBox(height: 12.v),
-              Padding(
-                padding: EdgeInsets.only(left: 8.h),
-                child: GestureDetector(
-                  onTap: () {
-                    FocusScope.of(context).requestFocus(nameFocus);
-                  },
-                  child: Center(
-                    // Zentriere den Text im Container
-                    child: CustomTextFormField(
-                      controller: nameController,
-                      hintText: "Spitzname",
-                      textInputAction: TextInputAction.done,
-                      focusNode: nameFocus,
-                      autofocus: false,
-                    ),
-                  ),
+            ),
+          ),
+          SizedBox(height: 12.v),
+          Padding(
+            padding: EdgeInsets.only(left: 8.h),
+            child: GestureDetector(
+              onTap: () {
+                FocusScope.of(context).requestFocus(nameFocus);
+              },
+              child: Center(
+                child: CustomTextFormField(
+                  controller: nameController,
+                  hintText: "Spitzname",
+                  textInputAction: TextInputAction.done,
+                  focusNode: nameFocus,
+                  autofocus: false,
                 ),
               ),
-              SizedBox(height: 13.v),
-              CustomOutlinedButton(
-                text: "Beitreten",
-                margin: EdgeInsets.only(left: 8.h),
-                onPressed: () {
-                  onTapBeitreten(context);
-                },
-                decoration: BoxDecoration(
-                  color: Colors.orange,
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-              ),
-              SizedBox(height: 15.v),
-              CustomOutlinedButton(
-                  text: "Zurück",
-                  margin: EdgeInsets.only(left: 8.h),
-                  buttonStyle: CustomButtonStyles.outlinePrimaryTL20,
-                  buttonTextStyle: theme.textTheme.displaySmall!,
-                  onPressed: () {
-                    onTapZurck(context);
-                  }),
-              SizedBox(height: 31.v),
-              Text("Hilfe", style: theme.textTheme.titleLarge)
-            ])));
+            ),
+          ),
+          SizedBox(height: 13.v),
+          CustomOutlinedButton(
+            text: "Beitreten",
+            margin: EdgeInsets.only(left: 8.h),
+            onPressed: () {
+              if (roomNumberController.text.isNotEmpty &&
+                  nameController.text.isNotEmpty) {
+                onTapBeitreten(context);
+              }
+            },
+            decoration: BoxDecoration(
+              color: Colors.orange,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          ),
+          SizedBox(height: 15.v),
+          CustomOutlinedButton(
+            text: "Zurück",
+            margin: EdgeInsets.only(left: 8.h),
+            buttonStyle: CustomButtonStyles.outlinePrimaryTL20,
+            buttonTextStyle: theme.textTheme.displaySmall!,
+            onPressed: () {
+              onTapZurck(context);
+            },
+          ),
+          SizedBox(height: 31.v),
+          Text("Hilfe", style: theme.textTheme.titleLarge),
+        ]),
+      ),
+    );
   }
 
   /// Navigates to the roomviewstudentScreen when the action is triggered.
-  onTapBeitreten(BuildContext context) {
-    Navigator.pushNamed(context, AppRoutes.roomviewstudentScreen);
+  Future<void> onTapBeitreten(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      // Wenn das Formular gültig ist, zeigen Sie das Popup an
+      await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return Center(
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 20.0),
+              child: AlertDialog(
+                title: Center(
+                  child: Text("Bestätigung"),
+                ),
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Center(
+                      child: Text(
+                        "In weiterer Folge werden ihre Standortdaten verwendet.",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    CustomOutlinedButton(
+                      text: "Bestätigen",
+                      onPressed: () {
+                        Navigator.of(context).pop(); // Popup schließen
+                        // Navigieren Sie zum roomviewstudentScreen
+                        Navigator.pushNamed(
+                            context, AppRoutes.roomviewstudentScreen);
+                      },
+                      decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      );
+    }
   }
 
   /// Navigates to the mainmenuScreen when the action is triggered.
--- lib/presentation/teacherlogin_screen/teacherlogin_screen.dart
+++ lib/presentation/teacherlogin_screen/teacherlogin_screen.dart
@@ -5,132 +5,170 @@ import 'package:klagenfurtquest_final/widgets/custom_text_form_field.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-// ignore_for_file: must_be_immutable
 class TeacherloginScreen extends StatelessWidget {
   TeacherloginScreen({Key? key}) : super(key: key);
 
   TextEditingController recMailController = TextEditingController();
-
   TextEditingController recPasswordController = TextEditingController();
+
+  FocusNode recMailFocus = FocusNode();
+  FocusNode recPasswordFocus = FocusNode();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-            resizeToAvoidBottomInset: false,
-            body: SizedBox(
-                height: SizeUtils.height,
-                width: double.maxFinite,
-                child: Stack(alignment: Alignment.bottomCenter, children: [
-                  Align(
-                      alignment: Alignment.topCenter,
-                      child: SizedBox(
-                          height: 498.v,
-                          width: double.maxFinite,
-                          child:
-                              Stack(alignment: Alignment.topCenter, children: [
-                            CustomImageView(
-                                imagePath: ImageConstant.imgAppbackground,
-                                height: 498.v,
-                                width: 360.h,
-                                alignment: Alignment.center),
-                            Align(
-                                alignment: Alignment.topCenter,
-                                child: Container(
-                                    width: 280.h,
-                                    margin: EdgeInsets.only(top: 20.v),
-                                    child: RichText(
-                                        text: TextSpan(children: [
-                                          TextSpan(
-                                              text: "Willkommen bei",
-                                              style: CustomTextStyles
-                                                  .displaySmallffffa500),
-                                          TextSpan(
-                                              text: "KlagenfurtQuest",
-                                              style: CustomTextStyles
-                                                  .displaySmallffffffff)
-                                        ]),
-                                        textAlign: TextAlign.left)))
-                          ]))),
-                  _buildTeacherLoginMenu(context)
-                ]))));
-  }
-
-  /// Section Widget
-  Widget _buildTeacherLoginMenu(BuildContext context) {
-    return Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 10.h, vertical: 16.v),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadiusStyle.customBorderTL25,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.2),
-                  spreadRadius: 2,
-                  blurRadius: 5,
-                  offset: Offset(0, 3),
-                ),
-              ],
-            ),
-            child: Column(mainAxisSize: MainAxisSize.min, children: [
-              SizedBox(
-                  width: 215.h,
-                  child: Text("Willkommen zurück!",
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                      textAlign: TextAlign.center,
-                      style: CustomTextStyles.displaySmallSemiBold)),
-              SizedBox(height: 18.v),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: SizedBox(
+          height: SizeUtils.height,
+          width: double.maxFinite,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
               Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                      padding: EdgeInsets.only(left: 18.h),
-                      child: Text("Schul E-Mail",
-                          style: theme.textTheme.titleLarge))),
-              Padding(
-                  padding: EdgeInsets.only(left: 8.h),
-                  child: CustomTextFormField(
-                    controller: recMailController,
-                    autofocus: false,
-                  )),
-              SizedBox(height: 15.v),
-              Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                      padding: EdgeInsets.only(left: 19.h),
-                      child:
-                          Text("Passwort", style: theme.textTheme.titleLarge))),
-              Padding(
-                  padding: EdgeInsets.only(left: 8.h),
-                  child: CustomTextFormField(
-                    controller: recPasswordController,
-                    textInputAction: TextInputAction.done,
-                    obscureText: true,
-                    autofocus: false,
-                  )),
-              SizedBox(height: 26.v),
-              CustomOutlinedButton(
-                text: "Anmelden",
-                margin: EdgeInsets.only(left: 8.h),
-                onPressed: () {
-                  onTapAnmelden(context);
-                },
-                decoration: BoxDecoration(
-                  color: Colors.orange,
-                  borderRadius: BorderRadius.circular(20.0),
+                alignment: Alignment.topCenter,
+                child: SizedBox(
+                  height: 498.v,
+                  width: double.maxFinite,
+                  child: Stack(
+                    alignment: Alignment.topCenter,
+                    children: [
+                      CustomImageView(
+                        imagePath: ImageConstant.imgAppbackground,
+                        height: 498.v,
+                        width: 360.h,
+                        alignment: Alignment.center,
+                      ),
+                      Align(
+                        alignment: Alignment.topCenter,
+                        child: Container(
+                          width: 280.h,
+                          margin: EdgeInsets.only(top: 20.v),
+                          child: RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: "Willkommen bei",
+                                  style: CustomTextStyles.displaySmallffffa500,
+                                ),
+                                TextSpan(
+                                  text: "KlagenfurtQuest",
+                                  style: CustomTextStyles.displaySmallffffffff,
+                                ),
+                              ],
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              SizedBox(height: 12.v),
-              Text("Hilfe", style: theme.textTheme.titleLarge),
-              SizedBox(height: 9.v)
-            ])));
+              _buildTeacherLoginMenu(context),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
-  /// Navigates to the teacherloggedinmenuScreen when the action is triggered.
-  //onTapAnmelden(BuildContext context) {
-  //Navigator.pushNamed(context, AppRoutes.teacherloggedinmenuScreen);
+  Widget _buildTeacherLoginMenu(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10.h, vertical: 16.v),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadiusStyle.customBorderTL25,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              width: 215.h,
+              child: Text(
+                "Willkommen zurück!",
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+                style: CustomTextStyles.displaySmallSemiBold,
+              ),
+            ),
+            SizedBox(height: 18.v),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.only(left: 18.h),
+                child: Text(
+                  "Schul E-Mail",
+                  style: theme.textTheme.titleLarge,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 8.h),
+              child: CustomTextFormField(
+                controller: recMailController,
+                focusNode: recMailFocus,
+                autofocus: false,
+                onEditingComplete: () {
+                  FocusScope.of(context).requestFocus(recPasswordFocus);
+                },
+              ),
+            ),
+            SizedBox(height: 15.v),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: EdgeInsets.only(left: 19.h),
+                child: Text(
+                  "Passwort",
+                  style: theme.textTheme.titleLarge,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 8.h),
+              child: CustomTextFormField(
+                controller: recPasswordController,
+                focusNode: recPasswordFocus,
+                textInputAction: TextInputAction.done,
+                obscureText: true,
+                autofocus: false,
+                onEditingComplete: () {
+                  onTapAnmelden(context);
+                },
+              ),
+            ),
+            SizedBox(height: 26.v),
+            CustomOutlinedButton(
+              text: "Anmelden",
+              margin: EdgeInsets.only(left: 8.h),
+              onPressed: () {
+                onTapAnmelden(context);
+              },
+              decoration: BoxDecoration(
+                color: Colors.orange,
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+            ),
+            SizedBox(height: 12.v),
+            Text("Hilfe", style: theme.textTheme.titleLarge),
+            SizedBox(height: 9.v),
+          ],
+        ),
+      ),
+    );
+  }
 
   Future<void> onTapAnmelden(BuildContext context) async {
     final String url = 'http://192.168.0.10:8080/login';
